590b021ec6125ea872782767e9808035fd32f556
Joram Wambugu <artisan254@gmail.com>
2021-08-05 06:40:17 +0300

    add get accounts test

--- api/account_test.go
+++ api/account_test.go
@@ -37,6 +37,17 @@ func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, account db.Accoun
 	require.Equal(t, account, gotAccount)
 }
 
+func requireBodyMatchAccounts(t *testing.T, body *bytes.Buffer, accounts []db.Account) {
+	data, err := ioutil.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotAccounts []db.Account
+
+	err = json.Unmarshal(data, &gotAccounts)
+	require.NoError(t, err)
+	require.Equal(t, accounts, gotAccounts)
+}
+
 func TestGetAccountAPI(t *testing.T) {
 	account := createRandomAccount()
 
@@ -218,6 +229,79 @@ func TestCreatAccountAPI(t *testing.T) {
 
 			request, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(requestBody))
 			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			testCase.checkResponse(t, recorder)
+		})
+	}
+}
+
+func TestGetAccounts(t *testing.T) {
+	n := 5
+
+	accounts := make([]db.Account, n)
+
+	for i := 0; i < 5; i++ {
+		accounts[i] = createRandomAccount()
+	}
+
+	type QueryParams struct {
+		PageID   int32
+		PageSize int32
+	}
+
+	testCases := []struct {
+		name          string
+		queryParams   QueryParams
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "StatusOK",
+			queryParams: QueryParams{
+				PageID:   1,
+				PageSize: int32(n),
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.ListAccountsParams{
+					Limit:  int32(n),
+					Offset: 0,
+				}
+				store.EXPECT().
+					ListAccounts(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(accounts, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchAccounts(t, recorder.Body, accounts)
+			},
+		},
+	}
+
+	for i := range testCases {
+		testCase := testCases[i]
+
+		t.Run(testCase.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			testCase.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			url := "/v1/accounts"
+
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
+
+			// Add query parameters
+			q := request.URL.Query()
+			q.Add("page_id", fmt.Sprintf("%d", testCase.queryParams.PageID))
+			q.Add("page_size", fmt.Sprintf("%d", testCase.queryParams.PageSize))
+			request.URL.RawQuery = q.Encode()
 
 			server.router.ServeHTTP(recorder, request)
 			testCase.checkResponse(t, recorder)
