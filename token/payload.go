5fa7f7e01cdb19a69f7e3af79cef7c8ac1363726
Joram Wambugu <artisan254@gmail.com>
2021-08-07 12:40:29 +0300

    JWT
    Generate and verify jwt tokens

---
+++ token/payload.go
@@ -0,0 +1,48 @@
+package token
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+// Different types of error returned by the VerifyToken function
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token has expired")
+)
+
+// Payload contains the payload data of the token
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
+
+// Valid checks if the token payload is valid or not
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiresAt) {
+		return ErrExpiredToken
+	}
+
+	return nil
+}
+
+// NewPayload creates a new token payload with a specific username and duration
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiresAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}
